c37c404a581259f8bea48f7927df966fc3b7df1f
Masataka Koike <plinecom@gmail.com>
2016-09-07 17:40:08 +0900

    test

---
+++ sample1.py
@@ -0,0 +1,15 @@
+
+file_path = "/root/exr/v001/V14_29_16A_rtc_v001.0001.exr"
+fp = open(file_path, "rb")
+
+i = 0
+for f in fp:
+    str = f.split('\0')
+    print str
+    i += 1
+    if i >= 10:
+        break
+
+
+#fp.readinto(_header)
+fp.close()

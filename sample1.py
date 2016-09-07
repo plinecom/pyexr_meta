9ef3f203b5a5e8fbc11869df6af551b8798294e6
Masataka Koike <plinecom@gmail.com>
2016-09-07 19:13:36 +0900

    test

--- sample1.py
+++ sample1.py
@@ -3,7 +3,9 @@ file_path = "/root/exr/v001/V14_29_16A_rtc_v001.0001.exr"
 fp = open(file_path, "rb")
 
 magic = "762F3101"
-
+version = "02"
+flag = "000000"
+# // A zero-length attribute name indicates the end of the header.
 i = 0
 for f in fp:
     str = f.split('\0')
